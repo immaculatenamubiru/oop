9f8f6758218bdc3008441af6af0e7b1b719aaea1
immaculatenamubiru <immaculatenamubiru07@gmail.com>
2024-10-04 16:39:17 +0300

    Add files via upload

---
+++ oop/do_while/src/do_while/do_while.java
@@ -0,0 +1,16 @@
+package do_while;
+
+public class do_while {
+
+	public static void main(String[] args) {
+		int value = 1;
+				
+		// do while loop
+		do {
+			System.out.println(+ value);
+			value++;
+		} while (value <= 8);
+
+	}
+
+}
